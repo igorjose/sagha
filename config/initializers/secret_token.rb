83a7166daf7b9440bc891bcd243e5e507d570e61
Rodolfo Sikora <sikora@gmail.com>
2013-06-06 14:48:51 -0300

    Initial repo with haml, unicorn and simple_form

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sagha::Application.config.secret_key_base = '0aad8c4fb4ef4eb0b8160455fb8de5b3c5d34b9152f45da4a9a97523798b88fe693d188e3f84faa5842b7a179696729f775c6d74485e48ffe17d806e8a888ab0'
