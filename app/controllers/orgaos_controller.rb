3773da619500fb2f9cd75cad93b34a5052b71250
Rodolfo Sikora <sikora@gmail.com>
2013-06-10 11:48:37 -0300

    adding br locale for devise and before_filter for controllers

--- app/controllers/orgaos_controller.rb
+++ app/controllers/orgaos_controller.rb
@@ -1,5 +1,7 @@
 class OrgaosController < ApplicationController
   before_action :set_orgao, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
+
 
   # GET /orgaos
   # GET /orgaos.json
