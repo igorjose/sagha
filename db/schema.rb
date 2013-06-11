59986cfc5ce56416f942142bd1f48a8377d96eee
Rodolfo Sikora <sikora@gmail.com>
2013-06-11 17:07:18 -0300

    adding admin tag for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130611181105) do
+ActiveRecord::Schema.define(version: 20130611195257) do
 
   create_table "desktops", force: true do |t|
     t.integer  "qtd_funcionando"
@@ -65,8 +65,8 @@ ActiveRecord::Schema.define(version: 20130611181105) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20130611181105) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "orgao_id"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
