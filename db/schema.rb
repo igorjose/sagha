33a0099a70e2af8dec35def0ac58cc2529325d7e
eltonsantos <eltonaxl@hotmail.com>
2013-06-13 09:17:17 -0300

    Atualizando repositorios

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,26 @@ ActiveRecord::Schema.define(version: 20130612161525) do
     t.integer  "orgao_id"
   end
 
+  create_table "informacoes_pessoais", force: true do |t|
+    t.integer  "cargo_id"
+    t.string   "nome"
+    t.string   "telefone"
+    t.string   "celular_cooperativo"
+    t.string   "celular_pessoal"
+    t.string   "email_cooperativo"
+    t.string   "email_pessoal"
+    t.string   "tipo_contratacao"
+    t.date     "vigencia_contrato"
+    t.boolean  "pode_renovar"
+    t.text     "mini_curriculo"
+    t.text     "experiencia"
+    t.text     "cursos"
+    t.text     "conhecimentos_adicionais"
+    t.text     "observacoes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "orgao_impressoras", force: true do |t|
     t.integer  "orgao_id"
     t.string   "tipo"
